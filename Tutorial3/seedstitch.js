b06a62b3c7082be30598615f9792cec7afb760e4
Jenny <jhl989@mit.edu>
2018-03-21 23:58:55 -0400

    Slight edit to helloworld.k for accurate carrier formatting, updated seedstitch.js to work with knitout-fronted update

--- Tutorial3/seedstitch.js
+++ Tutorial3/seedstitch.js
@@ -1,38 +1,42 @@
 // import the knitoutWriter code and instantiate it as an object
-var knitoutWriter = require('../knitout-frontend/knitoutWriter');
-k = new knitoutWriter();
+// passing in all eight carriers despite not using all of them
+const knitout = require('knitout');
+let k = new knitout.Writer({carriers:['1', '2', '3', '4', '5', '6', '7', '8']});
 
 // add some headers relevant to this job
 k.addHeader('Machine','SWGXYZ');
 k.addHeader('Gauge','15');
-k.addHeader('Carriers', '1');
 
 // swatch variables
-var height = 20;
-var width = 21; //want to put first stich on the front bed, hack for now
-var carrier = 6;
+var height = 30;
+var width = 41; 
+var carrier = '6';
+
+// make sure the very first stitch in on the front bed,
+// since the machine complains if its on the back
+var front = width%2;
 
 // bring in carrier using yarn inserting hook
-k.inhook(carrier); 
+k.inhook(carrier);
 
 
+//initial tuck cast-on
 for (var s=width; s>0; s--) {
-    if(s%2==0) {
-        k.tuck("-", "b"+s, carrier);
+    if(s%2==front) {
+        k.tuck("-", "f"+s, carrier);
     }
     else {
-        k.tuck("-", "f"+s, carrier);
+        k.tuck("-", "b"+s, carrier);
     }
 }
 
-//k.stitchNumber(5);
 //rib on way back, skip last of the tucks
 for (var s=2; s<=width; s++) {
-    if (s%2==0) {
-        k.knit("+", "b"+s, carrier);
+    if (s%2==front) {
+        k.knit("+", "f"+s, carrier);
     }
     else {
-        k.knit("+", "f"+s, carrier);
+        k.knit("+", "b"+s, carrier);
     }    
 }
 
@@ -40,40 +44,50 @@ for (var s=2; s<=width; s++) {
 k.releasehook(carrier);
 
 
-// knit some rows back and forth
-for (var h=0; h<height; h++) {
+// knit until we have the right swatch height
+var current_height = 0
+while (current_height<height) {
 	for (var s=width; s>0; s--) {
-        if (s%2==0) {
-            k.knit("-", "b"+s, carrier);
-        }
+		if (s%2==front) {
+			k.xfer("f"+s, "b"+s);
+		}
 		else {
-            k.knit("-", "f"+s, carrier);
-        }
+			k.xfer("b"+s, "f"+s);
+		}
 	}
-    for (var s=width; s>0; s--) {
-        if (s%2==0) {
-            k.xfer("b"+s, "f"+s);
-        }
+
+	for (var s=width; s>0; s--) {
+		if (s%2==front) {
+			k.knit("-", "b"+s, carrier);
+		}
 		else {
-            k.xfer("f"+s, "b"+s);
-        }
+			k.knit("-", "f"+s, carrier);
+		}
 	}
+	current_height++;
+
+	if (current_height >= height) {
+		break;
+	}
+
 	for (var s=1; s<=width; s++) {
-		if (s%2==0) {
-            k.knit("+", "f"+s, carrier);
-        }
+		if (s%2==front) {
+			k.xfer("b"+s, "f"+s);
+		}
 		else {
-            k.knit("+", "b"+s, carrier);
-        }
+			k.xfer("f"+s, "b"+s);
+		}
 	}
-    for (var s=1; s<=width; s++) {
-		if (s%2==0) {
-            k.xfer("f"+s, "b"+s);
-        }
+
+	for (var s=1; s<=width; s++) {
+		if (s%2==front) {
+			k.knit("+", "f"+s, carrier);
+		}
 		else {
-            k.xfer("b"+s, "f"+s);
-        }
+			k.knit("+", "b"+s, carrier);
+		}
 	}
+	current_height++;
 }
 
 // bring the yarn out with the yarn inserting hook
@@ -81,5 +95,6 @@ k.outhook(carrier);
 
 // write the knitout to a file called "out.k"
 k.write('output/seedstitch.k');
+
 
 
